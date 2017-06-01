03d678b1db4d78b67a3743b7adc6dbf316789765
lzq <18248392627@163.com>
2017-06-01 10:46:06 +0800

    增加指标检索

--- search_Engines/settings.py
+++ search_Engines/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'icm*)qe=0gymu%@fu9y_k67q^ap#83*o%#a*h=j%_*jg3@0abf'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -115,6 +115,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join('/home/lzq/', "search_Engines_web/static")
+TATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
