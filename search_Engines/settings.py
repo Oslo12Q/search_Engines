181a434f4b554a9d8326141f6c154ec20d9e4feb
Oslo <18248392627@163.com>
2017-07-03 16:42:43 +0800

    a

--- search_Engines/settings.py
+++ search_Engines/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join('/home/lzq/', "search_Engines_web/static")
+STATIC_ROOT = os.path.join('/home/lzq/', "search_Engines_web/static/test")
 STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
 
 STATICFILES_DIRS = (
